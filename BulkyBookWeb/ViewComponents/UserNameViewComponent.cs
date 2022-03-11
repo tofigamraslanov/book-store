de08807ea58ef027c3ff82973f2051d74bd29d5b
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2022-03-11 22:45:13 +0400

    Creating and Consuming View Components ✅

---
+++ BulkyBookWeb/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserNameViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+
+            var user = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value);
+
+            return View(user);
+        }
+    }
+}
